964e4ad3946d6c323d7fa9159aac44665d442f1e
Brandon Leventhal <bleventh@gmail.com>
2015-02-15 18:57:17 -0800

    change api to return array of hoots instead of k-v pairs

--- hootly_api/fake_data.rb
+++ hootly_api/fake_data.rb
@@ -1,28 +1,29 @@
 require 'json'
 
-result = `curl --data '' localhost:4567/newuser`
+result = `curl --data '' http://www.anappnooneneeds.com/newuser`
 user_id = JSON.parse(result)["user_id"]
 
-result = `curl localhost:4567/hootloot?user_id=#{user_id}`
+result = `curl http://www.anappnooneneeds.com/hootloot?user_id=#{user_id}`
 hootloot = JSON.parse(result)["hootloot"]
 
 
-`curl --form "image=@/Users/brandon/Desktop/img.png" --form user_id=#{user_id} --form lat=5 --form long=5 --form hoot_text="hello" localhost:4567/hoots`
+`curl --form "image=@/Users/brandon/Desktop/img.png" --form user_id=#{user_id} --form lat=5 --form long=5 --form hoot_text="hello" http://www.anappnooneneeds.com/hoots`
 
-result = `curl "localhost:4567/hoots?lat=5&long=5&user_id=#{user_id}"`
+result = `curl "http://www.anappnooneneeds.com/hoots?lat=5&long=5&user_id=#{user_id}"`
 hoots = JSON.parse(result)
+p hoots
 
 hoots.each do |hoot|
-   `curl --form text="this is a comment" --form post_id=#{hoot[0]} --form user_id=#{user_id} localhost:4567/comments`
+   `curl --form text="this is a comment" --form post_id=#{hoot[0]} --form user_id=#{user_id} http://www.anappnooneneeds.com/comments`
 end
 
-`curl --form comment_id=1 --form user_id=#{user_id} localhost:4567/commentsup`
-`curl --form post_id=1 --form user_id=#{user_id} localhost:4567/hootsup`
+`curl --form comment_id=1 --form user_id=#{user_id} http://www.anappnooneneeds.com/commentsup`
+`curl --form post_id=1 --form user_id=#{user_id} http://www.anappnooneneeds.com/hootsup`
 
-`curl --form comment_id=1 --form user_id=#{user_id} localhost:4567/commentsdown`
-`curl --form post_id=1 --form user_id=#{user_id} localhost:4567/hootsdown`
+`curl --form comment_id=1 --form user_id=#{user_id} http://www.anappnooneneeds.com/commentsdown`
+`curl --form post_id=1 --form user_id=#{user_id} http://www.anappnooneneeds.com/hootsdown`
 
-result = `curl "localhost:4567/comments?post_id=1&user_id=#{user_id}"`
+result = `curl "http://www.anappnooneneeds.com/comments?post_id=1&user_id=#{user_id}"`
 
 comments = JSON.parse(result)
 
